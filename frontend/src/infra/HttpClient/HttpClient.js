58053c6e28c7ba91cfd586f75f29816288274b51
lucaschier <ens-lucasschier@ugv.edu.br>
2023-05-10 17:50:51 -0300

    Ciclo de atualização dos Tokens #2

--- frontend/src/infra/HttpClient/HttpClient.js
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -1,4 +1,5 @@
 import { tokenService } from "../../services/auth/tokenService";
+import nookies from 'nookies';
 
 export async function HttpClient(fetchUrl, fetchOptions) {
   const options = {
@@ -21,28 +22,39 @@ export async function HttpClient(fetchUrl, fetchOptions) {
     .then(async (response) => {
       if(!fetchOptions.refresh) return response;
       if(response.status !== 401) return response;
+      console.log('Middleware: Rodar código para atualizar o token');
 
-    console.log('Middleware: Rodar código para atualizar o token');
+      const isServer = Boolean(fetchOptions?.ctx);
+      const currentRefreshToken = fetchOptions?.ctx?.req?.cookies['REFRESH_TOKEN_NAME'];
+    try {
+      const refreshResponse = await HttpClient('http://localhost:3000/api/refresh', {
+        method: isServer ? 'PUT' : 'GET',
+        body: isServer ? { refresh_token: currentRefreshToken } : undefined,
+      });
+      const newAccessToken = refreshResponse.body.data.access_token;
+      const newRefreshToken = refreshResponse.body.data.refresh_token;
 
-    // [Tentar atualizar os tokens]
-    const refreshResponse = await HttpClient('http://localhost:3000/api/refresh', {
-      method: 'GET'
-    });
-    const newAccessToken = refreshResponse.body.data.access_token;
-    const newRefreshToken = refreshResponse.body.data.refresh_token;
-
-    // [Guarda os tokens]
-    tokenService.save(newAccessToken);
-
-    // [Tentar rodar o request anterior]
-    const retryResponse = await HttpClient(fetchUrl, {
-      ...options,
-      refresh: false,
-      headers: {
-        'Authorization': `Bearer ${newAccessToken}`
+      // [Guarda os tokens]
+      if(isServer) {
+        nookies.set(fetchOptions.ctx, 'REFRESH_TOKEN_NAME', newRefreshToken, {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+        })
       }
-    })
-
-    return retryResponse;
+      tokenService.save(newAccessToken);
+      // [Tentar rodar o request anterior]
+      const retryResponse = await HttpClient(fetchUrl, {
+        ...options,
+        refresh: false,
+        headers: {
+          'Authorization': `Bearer ${newAccessToken}`
+        }
+      })
+      return retryResponse;
+    } catch(err) {
+      console.error(err);
+      return response;
+    }
   });
 }
